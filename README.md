0925ff035563a1a40412fb5f8d0dd1c154573f27
Jacob Dentes <jdentes10@gmail.com>
2024-07-05 17:59:19 -0400

    Added readme, more key aliases

---
+++ README.md
@@ -0,0 +1,194 @@
+## Game Blinder
+This is a simple app for creating challenging video game playthroughs.
+It allows binding keyboard and mouse inputs to turning the app window black.
+This allows, for example, turning the screen black each time the player moves or clicks.
+
+## Antivirus
+As an app with a sometimes-seethrough window that listens to global inputs, this app is
+understandably prone to being flagged by antivirus. The app only listens to registered
+inputs and never connects to the internet. That being said, you should not download an
+app that you do not trust. Read and build from source if you have the understanding, 
+otherwise use at your own discretion.
+
+## Settings
+To choose which inputs trigger the screen going black and decide how long the screen should
+be black for, you should place a 'config.txt' file in the same directory as the executable.
+The text file should be formatted in the following way. The first line is a comma-separated
+list of the inputs to trigger black screen. The second line is a decimal number for the
+number of seconds the screen will stay black for after each input. Here is an example config.txt:
+`
+w, a, s, d, leftbutton, leftshift, comma
+2.0
+`
+With this configuration, pressing any of keys in "WASD", left clicking, pressing left shift,
+or hitting the comma key will cause the app window to go black for 2 seconds.
+
+**Note:** Holding down keys will act like the key is pressed every frame. Holding down mouse
+buttons only registers as the inital click.
+
+### A comprehensive list of representations
+This section contains all accepted input strings for the config file. As a note, the "lsuper"
+and "rsuper" keys are the platform dependent super keys (for example, windows keys for windows
+and command keys for mac); they are aliased with "lwindows" and "lcommand", "rwindows" and "rcommand,"
+respectively.
+
+#### Keyboard keys
+- a 
+- b 
+- c 
+- d 
+- e 
+- f 
+- g 
+- h 
+- i 
+- j 
+- k 
+- l 
+- m 
+- n 
+- o 
+- p 
+- q 
+- r 
+- s 
+- t 
+- u 
+- v 
+- w 
+- x 
+- y 
+- z 
+- 0 
+- ) 
+- 1 
+- ! 
+- 2 
+- @ 
+- 3 
+- # 
+- 4 
+- $ 
+- 5 
+- % 
+- 6 
+- ^ 
+- 7 
+- & 
+- 8 
+- * 
+- 9 
+- ( 
+- ` 
+- ~ 
+- / 
+- ? 
+- < 
+- . 
+- > 
+- - 
+- _ 
+- ; 
+- : 
+- [ 
+- { 
+- ] 
+- } 
+- = 
+- + 
+- \ 
+- | 
+- ' 
+- "
+- backspace
+- tab
+- enter
+- escape
+- space
+- pageup
+- pagedown
+- end
+- home
+- left
+- up
+- right
+- down
+- insert
+- delete
+- lsuper
+- lwindows
+- lcommand
+- rsuper
+- rwindows
+- rcommand
+- numpad0
+- numpad1
+- numpad2
+- numpad3
+- numpad4
+- numpad5
+- numpad6
+- numpad7
+- numpad8
+- numpad9
+- f1
+- f2
+- f3
+- f4
+- f5
+- f6
+- f7
+- f8
+- f9
+- f10
+- f11
+- f12
+- f13
+- f14
+- f15
+- f16
+- f17
+- f18
+- f19
+- f20
+- f21
+- f22
+- f23
+- f24
+- numlock
+- scrolllock
+- capslock
+- leftshift
+- rightshift
+- leftcontrol
+- rightcontrol
+- leftalt
+- rightalt
+- back
+- forward
+- refresh
+- volumemute
+- volumedown
+- volumeup
+- medianext
+- mediaprevious
+- mediastop
+- mediaplay
+- backquote
+- slash
+- backslash
+- comma
+- period
+- minus
+- quotekey
+- semicolon
+- leftbracket
+- rightbracket
+- equal
+
+#### Mouse buttons
+- leftbutton
+- rightbutton
+- middlebutton
+- x1button
+- x2button
--- src/make_bind.rs
+++ src/make_bind.rs
@@ -3,9 +3,9 @@ use std::thread;
 
 use log::warn;
 
-use inputbot::{get_keybd_key, MouseButton};
+use inputbot::{get_keybd_key, KeybdKey, MouseButton};
 
-fn make_keybind(inputs: &'static Arc<Mutex<u8>>, s: &str) {
+fn make_char_keybind(inputs: &'static Arc<Mutex<u8>>, s: &str) {
     match s.to_string().chars().next() {
         Some(c) => match get_keybd_key(c) {
             Some(kbd_key) => {
@@ -31,23 +31,128 @@ fn get_mousebd_button(s: &str) -> Option<MouseButton> {
     }
 }
 
-fn make_mousebind(inputs: &'static Arc<Mutex<u8>>, s: &str) {
+fn get_keybd_longkey(s: &str) -> Option<KeybdKey> {
+    match s.to_lowercase().as_str() {
+        "backspace" => Some(KeybdKey::BackspaceKey),
+        "tab" => Some(KeybdKey::TabKey),
+        "enter" => Some(KeybdKey::EnterKey),
+        "escape" => Some(KeybdKey::EscapeKey),
+        "space" => Some(KeybdKey::SpaceKey),
+        "pageup" => Some(KeybdKey::PageUpKey),
+        "pagedown" => Some(KeybdKey::PageDownKey),
+        "end" => Some(KeybdKey::EndKey),
+        "home" => Some(KeybdKey::HomeKey),
+        "left" => Some(KeybdKey::LeftKey),
+        "up" => Some(KeybdKey::UpKey),
+        "right" => Some(KeybdKey::RightKey),
+        "down" => Some(KeybdKey::DownKey),
+        "insert" => Some(KeybdKey::InsertKey),
+        "delete" => Some(KeybdKey::DeleteKey),
+        "lsuper" | "lwindows" | "lcommand" => Some(KeybdKey::LSuper),
+        "rsuper" | "rwindows" | "rcommand" => Some(KeybdKey::RSuper),
+        "numpad0" => Some(KeybdKey::Numpad0Key),
+        "numpad1" => Some(KeybdKey::Numpad1Key),
+        "numpad2" => Some(KeybdKey::Numpad2Key),
+        "numpad3" => Some(KeybdKey::Numpad3Key),
+        "numpad4" => Some(KeybdKey::Numpad4Key),
+        "numpad5" => Some(KeybdKey::Numpad5Key),
+        "numpad6" => Some(KeybdKey::Numpad6Key),
+        "numpad7" => Some(KeybdKey::Numpad7Key),
+        "numpad8" => Some(KeybdKey::Numpad8Key),
+        "numpad9" => Some(KeybdKey::Numpad9Key),
+        "f1" => Some(KeybdKey::F1Key),
+        "f2" => Some(KeybdKey::F2Key),
+        "f3" => Some(KeybdKey::F3Key),
+        "f4" => Some(KeybdKey::F4Key),
+        "f5" => Some(KeybdKey::F5Key),
+        "f6" => Some(KeybdKey::F6Key),
+        "f7" => Some(KeybdKey::F7Key),
+        "f8" => Some(KeybdKey::F8Key),
+        "f9" => Some(KeybdKey::F9Key),
+        "f10" => Some(KeybdKey::F10Key),
+        "f11" => Some(KeybdKey::F11Key),
+        "f12" => Some(KeybdKey::F12Key),
+        "f13" => Some(KeybdKey::F13Key),
+        "f14" => Some(KeybdKey::F14Key),
+        "f15" => Some(KeybdKey::F15Key),
+        "f16" => Some(KeybdKey::F16Key),
+        "f17" => Some(KeybdKey::F17Key),
+        "f18" => Some(KeybdKey::F18Key),
+        "f19" => Some(KeybdKey::F19Key),
+        "f20" => Some(KeybdKey::F20Key),
+        "f21" => Some(KeybdKey::F21Key),
+        "f22" => Some(KeybdKey::F22Key),
+        "f23" => Some(KeybdKey::F23Key),
+        "f24" => Some(KeybdKey::F24Key),
+        "numlock" => Some(KeybdKey::NumLockKey),
+        "scrolllock" => Some(KeybdKey::ScrollLockKey),
+        "capslock" => Some(KeybdKey::CapsLockKey),
+        "leftshift" => Some(KeybdKey::LShiftKey),
+        "rightshift" => Some(KeybdKey::RShiftKey),
+        "leftcontrol" => Some(KeybdKey::LControlKey),
+        "rightcontrol" => Some(KeybdKey::RControlKey),
+        "leftalt" => Some(KeybdKey::LAltKey),
+        "rightalt" => Some(KeybdKey::RAltKey),
+        "back" => Some(KeybdKey::BrowserBackKey),
+        "forward" => Some(KeybdKey::BrowserForwardKey),
+        "refresh" => Some(KeybdKey::BrowserRefreshKey),
+        "volumemute" => Some(KeybdKey::VolumeMuteKey),
+        "volumedown" => Some(KeybdKey::VolumeDownKey),
+        "volumeup" => Some(KeybdKey::VolumeUpKey),
+        "medianext" => Some(KeybdKey::MediaNextTrackKey),
+        "mediaprevious" => Some(KeybdKey::MediaPrevTrackKey),
+        "mediastop" => Some(KeybdKey::MediaStopKey),
+        "mediaplay" => Some(KeybdKey::MediaPlayPauseKey),
+        "backquote" => Some(KeybdKey::BackquoteKey),
+        "slash" => Some(KeybdKey::SlashKey),
+        "backslash" => Some(KeybdKey::BackslashKey),
+        "comma" => Some(KeybdKey::CommaKey),
+        "period" => Some(KeybdKey::PeriodKey),
+        "minus" => Some(KeybdKey::MinusKey),
+        "quotekey" => Some(KeybdKey::QuoteKey),
+        "semicolon" => Some(KeybdKey::SemicolonKey),
+        "leftbracket" => Some(KeybdKey::LBracketKey),
+        "rightbracket" => Some(KeybdKey::RBracketKey),
+        "equal" => Some(KeybdKey::EqualKey),
+        _ => None,
+    }
+}
+
+fn make_mousebind(inputs: &'static Arc<Mutex<u8>>, s: &str) -> bool {
     match get_mousebd_button(s) {
         Some(mse_button) => {
             mse_button.bind(|| {
                 let mut num = inputs.lock().unwrap();
                 *num = 1;
             });
+            true
         }
-        None => warn!("Unknown button: {}", s),
+        None => false,
+    }
+}
+
+fn make_longkeybind(inputs: &'static Arc<Mutex<u8>>, s: &str) -> bool {
+    match get_keybd_longkey(s) {
+        Some(kbd_key) => {
+            kbd_key.bind(|| {
+                let mut num = inputs.lock().unwrap();
+                *num = 1;
+            });
+            true
+        }
+        None => false,
     }
 }
 
 pub fn make_binds(inputs: &'static Arc<Mutex<u8>>, triggers: &[String]) {
     for s in triggers {
         match s.len() {
-            1 => make_keybind(inputs, s),
-            2.. => make_mousebind(inputs, s),
+            1 => make_char_keybind(inputs, s),
+            2.. => {
+                if !(make_mousebind(inputs, s) || make_longkeybind(inputs, s)) {
+                    warn!("Unknown bind: {}", s);
+                }
+            }
             0 => warn!("Make binds given empty string. This is a bug."),
         }
     }
